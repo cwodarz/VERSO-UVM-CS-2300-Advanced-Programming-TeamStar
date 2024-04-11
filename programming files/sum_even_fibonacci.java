3a9597211365e67ba8fe6777e3297543d93f64ce
Erik Arnold <143435574+earn2201@users.noreply.github.com>
2024-04-11 11:06:48 -0400

    fixed sum_even_fibonacci.java

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -6,10 +6,10 @@ public class sum_even_fibonacci {
         int a = 0, b = 1;
         int sumEven = 0;
         while (b < 4000000) {
-            if (b % 1 == 0) {  // should be: if (b % 2 == 0) {
+            if if (b % 2 == 0) {  // should be: if (b % 2 == 0) {
                 sumEven += b;
             }
-            int temp = a;  // should be int temp = b;
+            int temp = b;  // should be int temp = b;
             b = a + b;
             a = temp;
         }
