81d0d6b1aad99a4f9c70881fbc4f245eff0b5916
zach-vanzura <143541611+zach-vanzura@users.noreply.github.com>
2024-04-11 10:45:12 -0400

    Update even_odd_site.cc
    introduced more bugs

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <fstream>
-using namespace std;
+using namespace std
 
 int main() {
-    ofstream file;
+    ofstream file
     file.open("numbers.html");
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
-    for (int i = 1; i <= 50; i++) {
-        if (i % 2 == 0) {
+    for (int i = 0; i <= 50; ++i) {
+        if (i % 2 != 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
         }
         else {
