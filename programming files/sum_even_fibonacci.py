bd849d244cafd82776c71737829eb0385ce5b3fd
Julia Sober <143358195+julia-sober@users.noreply.github.com>
2024-04-11 10:33:59 -0400

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,8 +1,9 @@
+# BUG INTRODUCED
 a, b = 0, 1
 sum_even = 0
 while b < 4000000:
     # check if even
-    if b % 2 == 0:
+    if b % 3 == 0:
         sum_even += b
     # a becomes b, b becomes sum of a and b
     a, b = b, a+b
