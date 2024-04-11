c8cecd68aa0c57e098067a94dfc3eea0289752b5
cwodarz <112415123+cwodarz@users.noreply.github.com>
2024-04-11 10:38:46 -0400

    Update datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,8 +1,9 @@
+# bugs introduced by cam
 # this file takes a date in string form and formats it to be printed
 from datetime import datetime
 
 date_str = "2022-03-17 10:45:30"
-date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
+date_obj = datetime.strptime(date_str, '%Y-m-%d %H:%M:%S') # removed the % before the m 
+formattted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')  # added an extra t in formatted
 
 print(formatted_date)
