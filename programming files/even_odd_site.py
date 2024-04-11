e709d79bdc19a55b54cd20d5c74128b6d517134a
cwodarz <112415123+cwodarz@users.noreply.github.com>
2024-04-11 10:40:49 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,4 +1,5 @@
-with open("numbers.html", "w") as f:
+# bugs added by cam
+with open("numbers.html", "r") as f:
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
     for i in range(1, 50):
@@ -8,6 +9,7 @@ with open("numbers.html", "w") as f:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
 
-with open("numbers.html", "r") as f:
+with open("numbers.html", "w") as f:
     print(f.read())
+
     
